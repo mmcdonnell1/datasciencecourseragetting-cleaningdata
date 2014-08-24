c16f6d6a52fafcde6c3eb7e8241e7fe47b8acb4d
mmcdonnell1 <mmcdonnell_1@msn.com>
2014-08-24 00:07:31 -0400

    Updating the run_analysis.R script and the CodeBook.txt file in the repo.

--- run_analysis.R
+++ run_analysis.R
@@ -7,38 +7,32 @@
   ## Load training and test datasets
   
   X_train <- read.table("./UCI HAR Dataset/train/X_train.txt")
-  y_train <- read.table("./UCI HAR Datasettrain/y_train.txt")
+  y_train <- read.table("./UCI HAR Dataset/train/y_train.txt")
   subject_train <- read.table("./UCI HAR Dataset/train/subject_train.txt")
   X_test <- read.table("./UCI HAR Dataset/test/X_test.txt")
   y_test <- read.table("./UCI HAR Dataset/test/y_test.txt")
   subject_test <- read.table("./UCI HAR Dataset/test/subject_test.txt")
+  
+  ## Load feature labels and activity labels
+  
+  features <- read.table("./UCI HAR Dataset/features.txt")
+  activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
   
   ## Combine training and test datasets
   
   X <- rbind(X_train, X_test)
   y <- rbind(y_train, y_test)
   subject <- rbind(subject_train, subject_test)
-  
-  ## Load feature labels and activity labels
-  
-  features <- read.table("./UCI HAR Dataset/features.txt")
-  activity_labels <- read.table("./UCI HAR Dataset/activity_labels.txt")
-
-## 4. Appropriately labels the data set with descriptive variable names. 
 
   ## Add column names to the subject ids, activity ids, activity labels,
   ## and the features data
   
-  colnames(subject) <- c("subjectid")
-  colnames(y) <- c("activityid")
-  colnames(activity_labels) <- c("activityid", "activity_labels")
+  colnames(subject) <- c("subject_id")
+  colnames(y) <- c("activity_id")
+  colnames(activity_labels) <- c("activity_id", "activity_labels")
   colnames(X) <- features[, 2]
   
 ## 2. Extracts only the measurements on the mean and standard deviation for each measurement.
-
-  ## Identify and select the mean and std features
-
-  ## rm(featuresInclude) <- features[grep("mean[()]|std[()]",features[,2]),]
 
   ## Extract the columns related to mean and std features
 
@@ -55,10 +49,48 @@
   XInclude.subject.y <- cbind(XInclude.subject, y)
 
   ## Match each activity id with the appropriate activity label
-  
-  XInclude.subject.y.ActNames <- merge(XInclude.subject.y, activity_labels, 
-                                       by.x = "activityid", by.y= "activityid")
 
+  XInclude.subject.y.ActNames <- merge(XInclude.subject.y, activity_labels, 
+                                       by.x = "activity_id", by.y= "activity_id")
+
+## 4. Appropriately labels the data set with descriptive variable names.
+  
+  ## Extract the feature rows related to mean and std features
+
+  featuresInclude <- features[grep("mean[()]|std[()]",features[,2]), ]
+
+  ## Create space between label descriptors using underscores
+
+  featuresInclude$V3 <- gsub("Body", "_Body_", featuresInclude[,2])
+  featuresInclude$V3 <- gsub("Gravity", "_Gravity_", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("Jerk", "_Jerk", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("Mag", "_Magnitude", featuresInclude[,3])
+
+  ## Convert -X, -Y, and -Z to include "Axis" reference
+
+  featuresInclude$V3 <- gsub("-X", "_X_Axis", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("-Y", "_Y_Axis", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("-Z", "_Z_Axis", featuresInclude[,3])
+
+  ## Convert "time" and "frequency" indicators to complete values
+
+  featuresInclude$V3 <- gsub("t_", "Time_", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("f_", "Frequency_", featuresInclude[,3])
+
+  ## Repalce R-unfriendly characters (i.e. "-" and "()") and clean-up incorrect names
+
+  featuresInclude$V3 <- gsub("-", "_", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("\\()", "", featuresInclude[,3])
+  featuresInclude$V3 <- gsub("__Body", "", featuresInclude[,3])
+
+  ## Convert variable names to lower case
+
+  featuresInclude$V3 <- tolower(featuresInclude[,3])
+  
+  ## Apply new tidy variable names
+  
+  colnames(XInclude.subject.y.ActNames) <- c("activity_id", featuresInclude[,3], "subject_id", "activity_labels")
+  
 ## 5. Creates a second, independent tidy data set with the average of each
 ## variable for each activity and each subject.
 
@@ -70,7 +102,7 @@
   ## Calculate column averages for each subject and activity
 
   XInclude.colMeans.by.Subject.Activity <- ddply(XInclude.subject.y.ActNames,
-                                                 .(subjectid, activityid, activity_labels),
+                                                 .(subject_id, activity_id, activity_labels),
                                                  colwise(mean))
   ## Create tidy data set
   
